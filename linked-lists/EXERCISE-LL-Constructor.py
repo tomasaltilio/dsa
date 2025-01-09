ef69bb77082d070e6593a24b2afe31fc6447f947
Tomas Altilio <tomas.altilio@gmail.com>
2025-01-09 09:35:58 -0300

    Update 8407987894712757316

--- linked-lists/EXERCISE-LL-Constructor.py
+++ linked-lists/EXERCISE-LL-Constructor.py
@@ -42,11 +42,11 @@ class LinkedList:
         return None
 
       # llegar hasta el ultimo, sacarlo y cambiar el tail
-      current = self.head
+      tmp = self.head
       pre = self.head
-      while current.next is not None:
-        pre = current
-        current = current.next
+      while tmp.next is not None:
+        pre = tmp
+        tmp = tmp.next
 
       self.tail = pre
       self.tail.next = None
@@ -55,34 +55,49 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+      return tmp
+
     def prepend(self, value):
       new_node = Node(value)
       if self.head is None:
         self.head = new_node
-        self.tail
+        self.tail = new_node
+      else:
+        new_node.next = self.head
+        self.head = new_node
+      self.length += 1
+      return True
 
+    def pop_first(self):
+      if self.head is None:
+        return None
 
-      new_node.next = self.head
-      self.head = new_node
+      tmp = self.head
+      if self.length == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.next
 
+      self.length -= 1
+      if self.length == 0:
+        self.head = None
+        self.tail = None
 
+      return tmp
 
+    def get(self, index):
+      if index < 0 or index >= self.length:
+        return None
+
+      current = self.head
+
+      for _ in range(index):
+        current = current.next
+
+      return current.value
 
 my_linked_list = LinkedList(4)
 
 my_linked_list.append(5)
 my_linked_list.append(6)
-my_linked_list.append(7)
-my_linked_list.append(8)
-
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
-
-
-my_linked_list.prepend(0)
-
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
-my_linked_list.print_list()
