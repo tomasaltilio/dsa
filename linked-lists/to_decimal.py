453a07ae54b4fbb8cd414c91095d713792b46eb7
Tomas Altilio <tomas.altilio@gmail.com>
2025-01-22 12:38:06 -0300

    binary to decimal method in lls

---
+++ linked-lists/to_decimal.py
@@ -0,0 +1,126 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+
+    def print_list(self):
+        if self.head is None:
+            print("empty list")
+        else:
+            temp = self.head
+            values = []
+            while temp is not None:
+                values.append(str(temp.value))
+                temp = temp.next
+            print(" -> ".join(values))
+
+    def binary_to_decimal(self):
+      '''
+      The binary_to_decimal method should start from the head of the linked list and
+      use each node's value to calculate the corresponding decimal number. The formula
+      to convert a binary number to decimal is as follows:
+
+      To put it in simple terms, each digit of the binary number is multiplied by 2
+      raised to the power equivalent to the position of the digit, counting from right
+      to left starting from 0, and all the results are summed together to get the
+      decimal number.
+
+      The binary_to_decimal method should return this calculated decimal number.
+      '''
+
+      res = 0
+      pos = self.length - 1
+      current = self.head
+
+      while current is not None:
+        res += current.value * (2 ** pos)
+        current = current.next
+        pos -= 1
+
+      return res
+
+    1*(2**0) + 1*(2**1) + 0*(2**2)
+    1 + 2
+
+# Test case 1: Binary number 110 = Decimal number 6
+linked_list = LinkedList(1)
+linked_list.append(1)
+linked_list.append(0)
+result = linked_list.binary_to_decimal()
+
+try:
+    assert result == 6
+    print("Test case 1 passed, returned: ", result)
+except AssertionError:
+    print("Test case 1 failed, returned: ", result)
+
+# Test case 2: Binary number 1000 = Decimal number 8
+linked_list = LinkedList(1)
+linked_list.append(0)
+linked_list.append(0)
+linked_list.append(0)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 8
+    print("Test case 2 passed, returned: ", result)
+except AssertionError:
+    print("Test case 2 failed, returned: ", result)
+
+# Test case 3: Binary number 0 = Decimal number 0
+linked_list = LinkedList(0)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 0
+    print("Test case 3 passed, returned: ", result)
+except AssertionError:
+    print("Test case 3 failed, returned: ", result)
+
+# Test case 4: Binary number 1 = Decimal number 1
+linked_list = LinkedList(1)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 1
+    print("Test case 4 passed, returned: ", result)
+except AssertionError:
+    print("Test case 4 failed, returned: ", result)
+
+# Test case 5: Binary number 1101 = Decimal number 13
+linked_list = LinkedList(1)
+linked_list.append(1)
+linked_list.append(0)
+linked_list.append(1)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 13
+    print("Test case 5 passed, returned: ", result)
+except AssertionError:
+    print("Test case 5 failed, returned: ", result)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Test case 1 passed, returned:  6
+    Test case 2 passed, returned:  8
+    Test case 3 passed, returned:  0
+    Test case 4 passed, returned:  1
+    Test case 5 passed, returned:  13
+"""
+
